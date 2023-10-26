9e480e1018921e10efc288ead4ffb437514426e7
jwk92 <jwk9284@gmail.com>
2023-10-26 13:43:16 +0900

    feat: main ui code update

--- utils/main_ui.py
+++ utils/main_ui.py
@@ -4,72 +4,54 @@ from PySide6.QtWidgets import *
 from PySide6.QtGui import *
 from PySide6.QtCore import *
 
-from .mouse_event import BboxDrawer
-from .json_loader import JsonLoader
-from .annotation import Annotator
 
-class Ui_Dialog(BboxDrawer, Annotator):
+from utils.canvas import Canvas
 
-    def __init__(self):
+class MainWindow(QMainWindow):
+
+    def __init__(self, ui_w=1200, ui_h=800):
         super().__init__()
+        self.setMinimumSize(ui_w, ui_h)
+        self.label_hist = []
+        
+        
+        if self.label_hist:
+            self.default_label = self.label_hist[0]
+            
+        # self.settings = Settings()
+        
+        self.canvas = Canvas(parent=self)
+        # self.canvas.set_drawing_shape_to_square(settings.get(SETTING_DRAW_SQUARE, False))
+        # self.canvas.newShape.connect(self.new_shape)
+        
+        self.main_layout = QHBoxLayout()
+        self.central_widget = QWidget(self)
+        
         self.filename=[]
         self.path=[]
         self.current_im=[]
         
-        self.ann_dict = {}
-        self.image_ann = [] # [{'iscrowd': 0, 'image_id': 724, 'bbox': [120.07, 71.83, 134.49, 153.08], 'category_id': 13, 'id': 268988}, {}, {}, ...]
-        
-        
-        self.temp_ann_dict = {}
-        
-        self.begin, self.destination = QPoint(), QPoint()
-        self.xh, self.yh = 0, 0
-        
-        self.obj_idx = 0
-        
         self.initUI()
-        self.imsp_w, self.imsp_h = self.width()*(2/3), self.height()
-        
-        
-        img_w, img_h = self.image.width(), self.image.height()
-        self.xh, self.yh = abs(self.imsp_w - img_w)//2 , abs(self.imsp_h - img_h)//2
-        
-        self.categories_list=['person', 'bicycle', 'car', 'motorcycle', 'airplane', 
-                         'bus', 'train', 'truck', 'boat', 'traffic light', 'fire hydrant', 
-                         'stop sign', 'parking meter', 'bench', 'bird', 'cat', 'dog', 'horse', 
-                         'sheep', 'cow', 'elephant', 'bear', 'zebra', 'giraffe', 'backpack', 
-                         'umbrella', 'handbag', 'tie', 'suitcase', 'frisbee', 'skis', 'snowboard', 
-                         'sports ball', 'kite', 'baseball bat', 'baseball glove', 'skateboard', 
-                         'surfboard', 'tennis racket', 'bottle', 'wine glass', 'cup', 'fork', 
-                         'knife', 'spoon', 'bowl', 'banana', 'apple', 'sandwich', 'orange', 
-                         'broccoli', 'carrot', 'hot dog', 'pizza', 'donut', 'cake', 'chair', 
-                         'couch', 'potted plant', 'bed', 'dining table', 'toilet', 'tv', 
-                         'laptop', 'mouse', 'remote', 'keyboard', 'cell phone', 'microwave', 
-                         'oven', 'toaster', 'sink', 'refrigerator', 'book', 'clock', 'vase', 
-                         'scissors', 'teddy bear', 'hair drier', 'toothbrush']
-        
-        
+        self.show()
         
 
-    def initUI(self, ui_w=1200, ui_h=800):
-        self.setMinimumSize(ui_w, ui_h)
-        #########################################
-        #########################################
+    def initUI(self):
+        self.central_widget.setLayout(self.main_layout)
+        self.setCentralWidget(self.central_widget)
         
-        self.image_viewer = QVBoxLayout()
-        self.viewer= QLabel(self)
+        self.main_layout.addWidget(self.canvas)
+
+        self.canvas.mode = Canvas.CREATE
+        # self.canvas.set_editing(True)
         
-        self.image = QPixmap(820,780)
-        self.image.fill(Qt.gray)
-        
-        #########################################
-        #########################################
         #dataset list layout
         dset_loader = QVBoxLayout() # dataset loading part
         self.file_list = QListView()
         
         self.fileModel = QFileSystemModel()
         
+        label1 = QLabel('File list', self)
+        dset_loader.addWidget(label1)
         dset_loader.addWidget(self.file_list)
         
         pn_select = QHBoxLayout()
@@ -85,6 +67,8 @@ class Ui_Dialog(BboxDrawer, Annotator):
         pn_select.addWidget(self.nextButton)
         pn_select.addStretch(1)
         
+        
+        
         dset_loader.addLayout(pn_select)
         #########################################
         
@@ -93,14 +77,20 @@ class Ui_Dialog(BboxDrawer, Annotator):
         # ann obj list layout
         annlist_viewer = QVBoxLayout()
         self.obj_list = QListWidget()
+        
+        label2 = QLabel('Object list', self)
+        annlist_viewer.addWidget(label2)
         annlist_viewer.addWidget(self.obj_list)
         
         pn2_select = QHBoxLayout()
         self.saveButton = QPushButton('Save')
+        self.editButton = QPushButton('Edit')
         self.delButton = QPushButton('Del')
         
         pn2_select.addStretch(1)
         pn2_select.addWidget(self.saveButton)
+        pn2_select.addStretch(1)
+        pn2_select.addWidget(self.editButton)
         pn2_select.addStretch(1)
         pn2_select.addWidget(self.delButton)
         pn2_select.addStretch(1)
@@ -110,6 +100,8 @@ class Ui_Dialog(BboxDrawer, Annotator):
         
         ann_info = QVBoxLayout()
         self.class_list = QListWidget()
+        label3 = QLabel('Object class list', self)
+        ann_info.addWidget(label3)
         ann_info.addWidget(self.class_list)
         
         #########################################
@@ -121,14 +113,10 @@ class Ui_Dialog(BboxDrawer, Annotator):
         ann_tool.addLayout(ann_info)
         #########################################
         #########################################
-        
-        self.main_layout = QHBoxLayout()
-        self.main_layout.addLayout(self.image_viewer, 70)
+
+        self.main_layout.addWidget(self.canvas, 70)
         self.main_layout.addLayout(ann_tool, 30)
-        
-        self.setLayout(self.main_layout)
         self.retranslateUi()
-        self.show()
         
     
     def open_dialog_box(self):
@@ -140,6 +128,20 @@ class Ui_Dialog(BboxDrawer, Annotator):
         
         self.file_list.clicked.connect(self.on_clicked)
         
+        self.categories_list=['person', 'bicycle', 'car', 'motorcycle', 'airplane', 
+                         'bus', 'train', 'truck', 'boat', 'traffic light', 'fire hydrant', 
+                         'stop sign', 'parking meter', 'bench', 'bird', 'cat', 'dog', 'horse', 
+                         'sheep', 'cow', 'elephant', 'bear', 'zebra', 'giraffe', 'backpack', 
+                         'umbrella', 'handbag', 'tie', 'suitcase', 'frisbee', 'skis', 'snowboard', 
+                         'sports ball', 'kite', 'baseball bat', 'baseball glove', 'skateboard', 
+                         'surfboard', 'tennis racket', 'bottle', 'wine glass', 'cup', 'fork', 
+                         'knife', 'spoon', 'bowl', 'banana', 'apple', 'sandwich', 'orange', 
+                         'broccoli', 'carrot', 'hot dog', 'pizza', 'donut', 'cake', 'chair', 
+                         'couch', 'potted plant', 'bed', 'dining table', 'toilet', 'tv', 
+                         'laptop', 'mouse', 'remote', 'keyboard', 'cell phone', 'microwave', 
+                         'oven', 'toaster', 'sink', 'refrigerator', 'book', 'clock', 'vase', 
+                         'scissors', 'teddy bear', 'hair drier', 'toothbrush']
+        
         for idx, cls in enumerate(self.categories_list):
             self.class_list.insertItem(idx, cls)
     
@@ -149,12 +151,14 @@ class Ui_Dialog(BboxDrawer, Annotator):
         self.nextButton.clicked.connect(self.next_im)
         self.prevButton.clicked.connect(self.previous_im)
         
-        self.saveButton.clicked.connect(self.save_obj)
-        self.delButton.clicked.connect(self.remove_bbox)
+        # self.saveButton.clicked.connect(self.save_obj)
         
-        self.obj_list.clicked.connect(self.select_ann)
+        # self.editButton.clicked.connect(self.remove_obj)
+        
+        self.delButton.clicked.connect(self.remove_obj)
         
         
+    
     def next_im(self):
         # self.ann_temp_save()
         
@@ -200,7 +204,53 @@ class Ui_Dialog(BboxDrawer, Annotator):
             self.current_im=list_1[n-1]
             self.on_clicked_1()
             self.image_ann = []
+        
+        
+    def on_clicked(self, index):
+  
+        self.path = self.fileModel.fileInfo(index).absoluteFilePath()
+        self.canvas.load_pixmap(QPixmap(self.path))
+        self.current_im=self.path
+        self.obj_list.clear()
+        self.update()
+		
+    def on_clicked_1(self):
+        
+        self.path = self.current_im
+        self.canvas.load_pixmap(QPixmap(self.path))
+        self.obj_list.clear()
+        self.update()
+        
+        
+    def select_obj(self):
+        idx = 0
+        
+        self.canvas.shapes[idx]
+        self.canvas.select_shape()
+    
+    def de_select_obj(self):
+        self.canvas.de_select_shape()
+        
+        
+    def remove_obj(self):
+        
+        idx = self.obj_list.currentRow()
+        self.update_obj_list("remove", idx)
+        self.canvas.select_shape(self.canvas.shapes[idx])
+        self.canvas.delete_selected()
+
+        
+    def update_obj_list(self, action, idx=None):
+        
+        cls_idx = self.class_list.currentRow()
+        class_name = self.categories_list[cls_idx]
             
+        if action=="remove":
+            self.obj_list.takeItem(idx)
+            
+        elif action=="add":
+            self.obj_list.addItem(class_name)
+        
         
     
 
