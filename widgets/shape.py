c5f285a953f961f9e7faafefc0758d3b0b305753
jwk92 <jwk9284@gmail.com>
2024-11-05 17:40:27 +0900

    feat: code update

--- widgets/shape.py
+++ widgets/shape.py
@@ -8,39 +8,9 @@ from math import sqrt
 from PySide6 import QtCore
 from PySide6 import QtGui
 
+from widgets.utils import distance, img_arr_to_data, distancetoline
 
-def distance(p):
-    return sqrt(p.x() * p.x() + p.y() * p.y())
 
-def img_qt_to_arr(img_qt):
-    w, h, d = img_qt.size().width(), img_qt.size().height(), img_qt.depth()
-    bytes_ = img_qt.bits().asstring(w * h * d // 8)
-    img_arr = np.frombuffer(bytes_, dtype=np.uint8).reshape((h, w, d // 8))
-    return img_arr
-
-def img_pil_to_data(img_pil):
-    f = io.BytesIO()
-    img_pil.save(f, format="PNG")
-    img_data = f.getvalue()
-    return img_data
-
-def img_arr_to_data(img_arr):
-    img_pil = PIL.Image.fromarray(img_arr)
-    img_data = img_pil_to_data(img_pil)
-    return img_data
-
-def distancetoline(point, line):
-    p1, p2 = line
-    p1 = np.array([p1.x(), p1.y()])
-    p2 = np.array([p2.x(), p2.y()])
-    p3 = np.array([point.x(), point.y()])
-    if np.dot((p3 - p1), (p2 - p1)) < 0:
-        return np.linalg.norm(p3 - p1)
-    if np.dot((p3 - p2), (p1 - p2)) < 0:
-        return np.linalg.norm(p3 - p2)
-    if np.linalg.norm(p2 - p1) == 0:
-        return np.linalg.norm(p3 - p1)
-    return np.linalg.norm(np.cross(p2 - p1, p1 - p3)) / np.linalg.norm(p2 - p1)
 
 # TODO(unknown):
 # - [opt] Store paths instead of creating new ones at each paint.
