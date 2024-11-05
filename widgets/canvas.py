a6ab926ecebef03098d1a9b6c056f48fdc788f3c
jwk92 <jwk9284@gmail.com>
2024-11-05 17:42:47 +0900

    feat: code clean

--- widgets/canvas.py
+++ widgets/canvas.py
@@ -3,12 +3,8 @@ from PySide6 import QtCore
 from PySide6 import QtGui
 from PySide6 import QtWidgets
 
-
-QT5 = True
 from widgets.shape import Shape
 from widgets.utils import distance, find_box_coordinates, img_qt_to_arr
-from math import sqrt
-import numpy as np
 
 
 CURSOR_DEFAULT = QtCore.Qt.ArrowCursor
